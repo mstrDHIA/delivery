04ee878f1b4035d9da8110ed95cf9f1556de7b63
mstrDHIA <mstrdhia@gmail.com>
2021-04-10 00:53:38 +0100

    logout

--- lib/Providers/MenuProvider.dart
+++ lib/Providers/MenuProvider.dart
@@ -1,10 +1,12 @@
 import 'package:delivery_app_v0/Screens/AdminManage.dart';
 import 'package:delivery_app_v0/Screens/Report.dart';
+import 'package:delivery_app_v0/Screens/login.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../Widgets/popupMenu.dart';
 class MenuProvider extends ChangeNotifier {
 
-  Changeoption(BuildContext context,option){
+  Changeoption(BuildContext context,option) async {
     var result=option;
 
     notify();
@@ -14,6 +16,13 @@ class MenuProvider extends ChangeNotifier {
     if(result==MenuOptions.Settings){
       Navigator.push(context, MaterialPageRoute(builder: (context) => AdminManage(),));
     }
+    if(result==MenuOptions.Logout){
+       final prefs = await SharedPreferences.getInstance();  
+    prefs.setBool("logged", true).then((bool success) {
+      Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => Login(),),(route) => false,);
+      });
+    }
+
     print(option.toString());
   }
 
